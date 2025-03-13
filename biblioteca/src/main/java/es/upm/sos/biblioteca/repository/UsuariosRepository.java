4bcf6c84262b478f0da54363eb0488ed46acf869
alvariittoo7 <agalaperez2004@gmail.com>
2025-03-13 20:39:29 +0100

    Repository de Usuarios

--- biblioteca/src/main/java/es/upm/sos/biblioteca/repository/UsuariosRepository.java
+++ biblioteca/src/main/java/es/upm/sos/biblioteca/repository/UsuariosRepository.java
@@ -1,23 +1,28 @@
 package es.upm.sos.biblioteca.repository;
 import es.upm.sos.biblioteca.models.Usuario;
+
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface UsuariosRepository extends JpaRepository<Usuario, String> {
 
-    
-    // @Query("INSERT INTO Usuario (matricula, nombre, fechaNacimiento, correo) VALUES (':matricula', ':nombre', ':fecha', ':correo');")
-    // List<Usuario> parametros(@Param("matricula") int matricula, @Param("fecha") String fechaNacimiento, @Param("nombre") String nombre, @Param("correo") String correo);
+    //Obtener un usuario en base a su matricula
+    @Query(value = "SELECT u FROM Usuario u WHERE u.matricula = :matricula")
+    Usuario getUsuario(@Param("matricula") String matricula);
 
-    @Query("SELECT u FROM Usuario u WHERE u.matricula = :matricula")
-    List<Usuario> parametros(@Param("matricula") int matricula);
+    //Query para hacer update de algun dato de usuario
+    @Query("UPDATE Usuario u SET u.matricula = :matricula, u.nombre = :nombre, u.fecha = :fecha, u.correo = :correo")
+    int actualizarUsuario(@Param("matricula") String matricula, @Param("nombre") String nombre, @Param("fecha") String fecha, @Param("correo") String correo);
 
-    // @Query("UPDATE ")
-
+    //Para borrar un usuario segun su matricula
     @Query(value = "DELETE Usuario u WHERE u.matricula = :matricula")
     void deleteUsuario(@Param("matricula") String matricula);
 
-
+    //Query para obtener a todos los usuarios
     @Query("SELECT u FROM Usuario u")
-    List<Usuario> obtenerTodosLosUsuarios();
+    List<Usuario> getUsuarios();
 
 }
