a397543f1c9648928a49fa9bdb3c7b05e4ebab53
alvariittoo7 <agalaperez2004@gmail.com>
2025-03-13 20:38:38 +0100

    Logica de negocio de Usuarios

--- biblioteca/src/main/java/es/upm/sos/biblioteca/services/ServicioUsuarios.java
+++ biblioteca/src/main/java/es/upm/sos/biblioteca/services/ServicioUsuarios.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Service;
 
 import es.upm.sos.biblioteca.models.Usuario;
 import es.upm.sos.biblioteca.repository.LibrosRepository;
+import es.upm.sos.biblioteca.repository.UsuariosRepository;
 import lombok.*;
 
 @Service //Marcamos la clase como componente de servicio
@@ -17,6 +18,28 @@ public class ServicioUsuarios{
     @Autowired
 
     //repositorio al que llamamos para realizar las querys
-    private final LibrosRepository repository;
+    private final UsuariosRepository repository;
+
+     //metodo para actualizar/guardar un usuario en la bbdd
+    public Usuario postUsuario(Usuario u){
+        return repository.save(u);
+    }
+
+    //Metodo para obtener los datos de un usuario
+    public Optional<Usuario> getUsuario(String matricula){
+        return Optional.of(repository.getUsuario(matricula));
+    }
+    
+
+    //Metodo para obtener todos los usuarios | optional se usa para valores posibles null
+    public Optional<List<Usuario>> getUsuarios(){
+        return Optional.of(repository.getUsuarios());
+    }
+
+    //metodo delete
+    public void deleteUsuario(String matricula){
+        repository.deleteUsuario(matricula);
+    }
+
 
 }
